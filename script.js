7825f6b03993a38c7043d4e19b3780e2b0244c63
Pedro Ramos <opedroramosbr@gmail.com>
2023-05-22 13:19:21 -0300

    00

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-setTimeout(
+/*setTimeout(
     function tempo() {
         var elemento = document.querySelector('#esconder')
         
         elemento.style.display = "block";
         
-    }, 360000)
+    }, 360000)*/
     /*tempo em milisegundos*/
